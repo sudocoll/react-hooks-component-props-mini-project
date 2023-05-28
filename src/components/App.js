342c139d13f96290b366bc93b6b49d8d39053227
Collette Charles <collettecra123@gmail.com>
2023-05-28 09:43:43 -0400

    Complete Lab

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,9 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      <Header name= {blogData.name}/>
-      <About image= {blogData.image} about={blogData.about}/>
-      <ArticleList />      
+      <Header name={blogData.name}/>
+      <About image={blogData.image} about={blogData.about}/>
+      <ArticleList posts={blogData.posts}/>
     </div>
   );
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,17 +1,14 @@
 import React from "react";
-import blogData from "../data/blog";
-import Article from "./Article";
+import Article from "./Article"
 
-function ArticleList () {
-    const Post = blogData.posts
-    const article = Post.map((post) => {
-        return <Article key = {post.id} title = {post.title} date= {post.date} preview = {post.preview}></Article> 
-    })
-    return (
-        <main>
-            {article}
-        </main>
-    )
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map((post) => (
+        <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes}/>
+      ))}
+    </main>
+  );
 }
 
 export default ArticleList
